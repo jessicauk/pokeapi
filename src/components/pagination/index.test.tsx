944ae84ce76cb640de017573185659c8280af228
Jessica Apolinar <jessica_220691@hotmail.com>
2024-02-12 15:52:36 -0600

    pagination test

--- src/components/pagination/index.test.tsx
+++ src/components/pagination/index.test.tsx
@@ -2,19 +2,27 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Pagination from ".";
 
+const previous = jest.fn();
+const next = jest.fn();
+
 describe("Pagination", () => {
   it("should render the pagination", () => {
-    render(
-      <Pagination
-        previous={() => {}}
-        page={1}
-        total={10}
-        next={() => {}}
-      />
-    );
-    const previous = screen.getByText("Previous");
-    const next = screen.getByText("Next");
-    expect(previous).toBeInTheDocument();
-    expect(next).toBeInTheDocument();
+    render(<Pagination previous={previous} page={1} total={10} next={next} />);
+    const previousButton = screen.getByText("Previous");
+    const nextButton = screen.getByText("Next");
+    expect(previousButton).toBeInTheDocument();
+    expect(nextButton).toBeInTheDocument();
+  });
+  it("should call the previous function", () => {
+    render(<Pagination previous={previous} page={1} total={10} next={next} />);
+    const previousButton = screen.getByText("Previous");
+    previousButton.click();
+    expect(previous).toHaveBeenCalled();
+  });
+  it("should call the next function", () => {
+    render(<Pagination previous={previous} page={1} total={10} next={next} />);
+    const nextButton = screen.getByText("Next");
+    nextButton.click();
+    expect(next).toHaveBeenCalled();
   });
 });
